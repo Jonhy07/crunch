600a4cd055ef995cf0a57d3bba43786892b31ce2
@jsimaj <jsimaj@dmi-app.com>
2021-11-04 00:35:04 -0600

    cambio de allowed host

--- src/pool_energy_app/settings.py
+++ src/pool_energy_app/settings.py
@@ -35,7 +35,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = tuple (env.list('ALLOWED_HOSTS', default=[]))
+# ALLOWED_HOSTS = tuple (env.list('ALLOWED_HOSTS', default=[]))
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
